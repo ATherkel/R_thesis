c1363c2609e0c587efc4a2dc900e96a3859a0f42
ATherkel <at.bmxrider@gmail.com>
2016-11-23 21:09:05 +0100

    Update

--- Mean_excess.R
+++ Mean_excess.R
@@ -10,7 +10,7 @@
 
 bachfire <- readRDS("~/Year4/blok4/BachSkade/brand_cleaned.rds")
 udg <- with(bachfire,udg[udg>0])
-
+#udg[udg == min(udg)] <- 1
 
 sudg <- sort(udg,decreasing = TRUE)
 
@@ -31,7 +31,7 @@ source("Functions_Codan.R")
 
 par(mfrow = c(1,2))
 barplot(udg,main = "Claim sizes")
-hist(log(udg),50,main = "Histogram of log claim sizes",
+hist(log(udg),100,main = "Histogram of log claim sizes",
      xlab = "log claim",freq = FALSE)
 par(mfrow = c(1,1))
 
@@ -87,18 +87,30 @@ qqplot2(qcauchy,fitcauchy$estimate,udg)
 par(mfrow = c(1,2))
 qqplot2(qgamma,fitgam$estimate,log(udg))
 qqplot2(qnorm,fitnorm$estimate,log(udg))
+par(mfrow = c(1,1))
 
 ## ---- test ----
 
 a <- fitgam$estimate[1]
 b <- fitgam$estimate[2]
 
-
+par(mfrow = c(1,1))
 hist(log(udg),100,freq = FALSE)
-curve((dlgamma(x,fitlg$estimate[1],fitlg$estimate[2])),add = TRUE)
-curve((dgamma(x,fitgam$estimate[1],fitgam$estimate[2])),add = TRUE,col  =2)
+curve((dgamma(x,fitgam$estimate[1],fitgam$estimate[2])),add = TRUE,col = 4)
+curve((dnorm(x,fitnorm$estimate[1],fitnorm$estimate[2])),add = TRUE,col  =2)
+legend("topright",legend = c("Gamma fit","Gaussian fit"),col = c(3,2),lty = 1)
+par(mfrow = c(1,1))
 
+c(samplemean = mean(log(udg)), fitmean = unname(a/b))
+c(samplevar = var(log(udg)), fitvar = unname(a/b^2))
 
+## simulation
+sim <- lapply(rep(length(udg),3), rgamma, a,b)
+
+par(mfrow = c(2,2))
+hist(log(udg),100,main = "Histogram log data")
+sapply(sim, hist, breaks = 100, main = "Histogram gamma fit simulation")
+par(mfrow = c(1,1))
 
 
 ## ---- pot ----
